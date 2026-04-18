4e3a1589461cb8b9d1119098a51cadb6a4aeb12b
niwarthana-xgenlabs <niwarthana@xgenlabs.com.au>
2026-04-18 19:56:32 +0530

    feat: updated frontend URL in cors

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,15 @@ import { errorHandler } from './middleware/error.middleware';
 
 const app = express();
 
-app.use(cors({
-  origin:      process.env['FRONTEND_URL'] || 'http://localhost:5173',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'https://issuetrackapp.vercel.app',
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
